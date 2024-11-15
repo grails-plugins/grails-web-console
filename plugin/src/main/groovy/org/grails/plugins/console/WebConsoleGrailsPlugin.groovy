710fff4a70bd89312e2c9a0354b2b57c530f6cd5
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-11-15 11:37:22 -0800

    Fix version warning

--- plugin/src/main/groovy/org/grails/plugins/console/WebConsoleGrailsPlugin.groovy
+++ plugin/src/main/groovy/org/grails/plugins/console/WebConsoleGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class WebConsoleGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "7.0.0 > *"
+    def grailsVersion = "7.0.0-SNAPSHOT > *"
 
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
